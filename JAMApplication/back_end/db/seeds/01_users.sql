7322ad8fa6e14e84e3fa471988de76a8b8a32433
John <john.cloghesy@gmail.com>
2023-09-30 16:48:05 -0600

    Update users table - Remove username - Change sub_id to VARCHAR Update Seed

--- JAMApplication/back_end/db/seeds/01_users.sql
+++ JAMApplication/back_end/db/seeds/01_users.sql
@@ -3,7 +3,6 @@
 INSERT INTO users (user_id) VALUES (1);
 INSERT INTO users (first_name) VALUES ('Manpreet');
 INSERT INTO users (last_name) VALUES ('Singh');
-INSERT INTO users (username) VALUES ('MSingh');
 INSERT INTO users (email) VALUES ('manpreet.singh@gmail.com');
 INSERT INTO users (sub_id) VALUES (1);
 
@@ -11,7 +10,6 @@ INSERT INTO users (sub_id) VALUES (1);
 INSERT INTO users (user_id) VALUES (2);
 INSERT INTO users (first_name) VALUES ('John');
 INSERT INTO users (last_name) VALUES ('Cloghesy');
-INSERT INTO users (username) VALUES ('JCloghesy');
 INSERT INTO users (email) VALUES ('john.cloghesy@gmail.com');
 INSERT INTO users (sub_id) VALUES (2);
 b
