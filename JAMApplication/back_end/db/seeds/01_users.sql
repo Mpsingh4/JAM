ba9855623c0080142bb6af63ccdbba31026d3c11
John <john.cloghesy@gmail.com>
2023-09-30 16:38:44 -0600

    Seeding DB - users table

--- JAMApplication/back_end/db/seeds/01_users.sql
+++ JAMApplication/back_end/db/seeds/01_users.sql
@@ -1,3 +1,17 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- 1st user - Manpreet
+INSERT INTO users (user_id) VALUES (1);
+INSERT INTO users (first_name) VALUES ('Manpreet');
+INSERT INTO users (last_name) VALUES ('Singh');
+INSERT INTO users (username) VALUES ('MSingh');
+INSERT INTO users (email) VALUES ('manpreet.singh@gmail.com');
+INSERT INTO users (sub_id) VALUES (1);
+
+-- 2nd user - John
+INSERT INTO users (user_id) VALUES (2);
+INSERT INTO users (first_name) VALUES ('John');
+INSERT INTO users (last_name) VALUES ('Cloghesy');
+INSERT INTO users (username) VALUES ('JCloghesy');
+INSERT INTO users (email) VALUES ('john.cloghesy@gmail.com');
+INSERT INTO users (sub_id) VALUES (2);
+b
