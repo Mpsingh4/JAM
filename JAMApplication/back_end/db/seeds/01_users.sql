15d6a38fc4b65e4495453470ce39063a717db5df
John <john.cloghesy@gmail.com>
2023-09-30 23:55:33 -0600

    Implement DB seeding - Update users table & Create all other seed tables

--- JAMApplication/back_end/db/seeds/01_users.sql
+++ JAMApplication/back_end/db/seeds/01_users.sql
@@ -1,15 +1,7 @@
--- Users table seeds here (Example)
--- 1st user - Manpreet
-INSERT INTO users (user_id) VALUES (1);
-INSERT INTO users (first_name) VALUES ('Manpreet');
-INSERT INTO users (last_name) VALUES ('Singh');
-INSERT INTO users (email) VALUES ('manpreet.singh@gmail.com');
-INSERT INTO users (sub_id) VALUES (1);
+-- USERS TABLE SEED --
 
--- 2nd user - John
-INSERT INTO users (user_id) VALUES (2);
-INSERT INTO users (first_name) VALUES ('John');
-INSERT INTO users (last_name) VALUES ('Cloghesy');
-INSERT INTO users (email) VALUES ('john.cloghesy@gmail.com');
-INSERT INTO users (sub_id) VALUES (2);
-b
+-- Insert three users Manpreet, John, Jane
+INSERT INTO users (user_id, first_name, last_name, email, sub_id) VALUES
+  (1, 'Manpreet', 'Singh', 'manpreet.singh@gmail.com', 1),
+  (2, 'John', 'Cloghesy', 'john.cloghesy@gmail.com', 2),
+  (3, 'Jane', 'Doe', 'jane.doe@gmail.com', 3);
---
+++ JAMApplication/back_end/db/seeds/03_resumes.sql
@@ -0,0 +1,123 @@
+-- RESUMES TABLE --
+--   Inserts four resumes 1 x Manpreet, 2 x John, 1 x Jane --
+
+-- Manpreet's Resume (1) --
+INSERT INTO resumes (resume_id, user_id, resume_title, contents) VALUES
+  (11, 1, 'Two Page', 
+   'Manpreet Singh
+    New York, NY | 212-555-0149 | mrabb@email.com
+    Summary
+    Versatile Front-End Developer with 3+ years of experience designing, developing, and managing complex e-commerce sites and internal frameworks. Specializes in AngularJS and responsive design.
+    Education
+    Riverside University
+    Bachelor of Science in computer science
+    Experience
+    Crane & Jenkins, Front-End Developer
+    Jul. 2022 - Current
+    Revamped UI and app interface for customer-facing e-commerce site with 200,000+ unique visitors per month
+    Designed and developed front-end for 20+ websites, using jQuery, AJAX, RequireJS, and Handlebars.js
+    Created custom AngularJS components for internal framework
+    Automated image optimization, using Grunt and minified JS and CSS, which reduced page load times by up to 30%
+    Tradelot Technology, Front-End Developer
+    Aug. 2020 - Jul. 2022
+    Translated design team's UX wireframes and mockups into responsive, interactive features, using HTML/CSS and JavaScript
+    Worked with agile team to migrate legacy company website to AngularJS, Sass, HTML5, and Drupal
+    Created HTML email templates that improved email CTR rates by ~25%
+    Designed and developed GUI for applications and modules, using JavaScript
+    Skills
+    Won 1st place (out of 50+) at TradeLot's annual hackathon
+    Expert in responsive, mobile-first development
+    JavaScript (jQuery, AngularJS, React, Bootstrap), HTML/CSS, Git, Mocha, Jasmine'
+  );
+
+
+-- John's Resume (2)--
+INSERT INTO resumes (resume_id, user_id, resume_title, contents) VALUES
+  (21, 2, 'One Page', 
+   'John Cloghesy
+    New York, NY | 212-555-0149 | mrabb@email.com
+    Summary
+    Versatile Front-End Developer with 3+ years of experience designing, developing, and managing complex e-commerce sites and internal frameworks. Specializes in AngularJS and responsive design.
+    Education
+    Riverside University
+    Bachelor of Science in computer science
+    Experience
+    Crane & Jenkins, Front-End Developer
+    Jul. 2022 - Current
+    Revamped UI and app interface for customer-facing e-commerce site with 200,000+ unique visitors per month
+    Designed and developed front-end for 20+ websites, using jQuery, AJAX, RequireJS, and Handlebars.js
+    Created custom AngularJS components for internal framework
+    Automated image optimization, using Grunt and minified JS and CSS, which reduced page load times by up to 30%
+    Tradelot Technology, Front-End Developer
+    Aug. 2020 - Jul. 2022
+    Translated design team's UX wireframes and mockups into responsive, interactive features, using HTML/CSS and JavaScript
+    Worked with agile team to migrate legacy company website to AngularJS, Sass, HTML5, and Drupal
+    Created HTML email templates that improved email CTR rates by ~25%
+    Designed and developed GUI for applications and modules, using JavaScript
+    Skills
+    Won 1st place (out of 50+) at TradeLot's annual hackathon
+    Expert in responsive, mobile-first development
+    JavaScript (jQuery, AngularJS, React, Bootstrap), HTML/CSS, Git, Mocha, Jasmine'
+  ),
+  (22, 2, 'Two Page'
+   'John Cloghesy
+    Denver, CO | 303-555-0196 | emoore@email.com
+    Summary
+    Creative Senior Front-End Developer with 15+ years of experience leveraging JavaScript to build responsive websites, mobile apps, and interactive features that drive business growth and improve UX.
+    Education
+    Colorado State University
+    Bachelor of Science in computer science
+    Experience
+    Mountainside Tech, Senior Front-End Developer
+    Jul. 2019 - Current
+    Develop new UI functionality for multithreaded user-facing applications running on touchscreen devices, using AngularJS
+    Spearheaded redesign of company's flagship mobile app, which increased downloads by 28% in less than 6 months
+    Increased email signups 12% by creating new UI for a website landing page in React
+    Figure Ten Creative, Front-End Developer
+    Aug. 2015 - Jul. 2019
+    Built single-page applications with JavaScript on a custom-built framework
+    Leveraged AngularJS and JavaScript to build presentation layer for retail payment page
+    Developed and maintained critical components of e-commerce website, including shopping cart, quick view, and checkout page, using React components
+    River Tech, Front-End Developer
+    Jan. 2013 - Aug. 2015
+    Architected, designed, and developed intranet applications in Visual Studio 2005, using JavaScript
+    Introduced wireframing and information architecture concepts to increase team productivity
+    Created front-end prototype for a medical billing web platform that won company's Most Innovative award in 2007
+    Skills
+    Expert in HTML/CSS, JavaScript, jQuery, AngularJS, React
+    Knowledge of Python, Ruby, SQL, C#
+    Adobe Creative Suite (Photoshop, InDesign, Illustrator)'
+  );
+
+
+-- Jane's Resume (1) --
+INSERT INTO resumes (resume_id, user_id, resume_title, contents) VALUES
+  (31, 3, 'Three Page', 
+   'Jane Doe
+    Denver, CO | 303-555-0196 | emoore@email.com
+    Summary
+    Creative Senior Front-End Developer with 15+ years of experience leveraging JavaScript to build responsive websites, mobile apps, and interactive features that drive business growth and improve UX.
+    Education
+    Colorado State University
+    Bachelor of Science in computer science
+    Experience
+    Mountainside Tech, Senior Front-End Developer
+    Jul. 2019 - Current
+    Develop new UI functionality for multithreaded user-facing applications running on touchscreen devices, using AngularJS
+    Spearheaded redesign of company's flagship mobile app, which increased downloads by 28% in less than 6 months
+    Increased email signups 12% by creating new UI for a website landing page in React
+    Figure Ten Creative, Front-End Developer
+    Aug. 2015 - Jul. 2019
+    Built single-page applications with JavaScript on a custom-built framework
+    Leveraged AngularJS and JavaScript to build presentation layer for retail payment page
+    Developed and maintained critical components of e-commerce website, including shopping cart, quick view, and checkout page, using React components
+    River Tech, Front-End Developer
+    Jan. 2013 - Aug. 2015
+    Architected, designed, and developed intranet applications in Visual Studio 2005, using JavaScript
+    Introduced wireframing and information architecture concepts to increase team productivity
+    Created front-end prototype for a medical billing web platform that won company's Most Innovative award in 2007
+    Skills
+    Expert in HTML/CSS, JavaScript, jQuery, AngularJS, React
+    Knowledge of Python, Ruby, SQL, C#
+    Adobe Creative Suite (Photoshop, InDesign, Illustrator)'
+  );
