53cd778d65602a89d053c358243b76155f09ca9c
John <john.cloghesy@gmail.com>
2023-10-01 22:11:13 -0600

    Update DB Schema: Correct syntax - reminder_days responses table (add ,)

--- JAMApplication/back_end/db/schema/05_responses.sql
+++ JAMApplication/back_end/db/schema/05_responses.sql
@@ -7,6 +7,6 @@ CREATE TABLE responses (
   response_id         SERIAL PRIMARY KEY NOT NULL,
   app_id              INTEGER REFERENCES job_applications(app_id),
   interviewer_name    VARCHAR(255) NOT NULL,
-  reminder_days       INTEGER
+  reminder_days       INTEGER,
   note                VARCHAR
 );
