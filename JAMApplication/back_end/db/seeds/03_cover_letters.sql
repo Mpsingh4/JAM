6e2636db0ea92418659a827490d5e9dfa22a7532
John <john.cloghesy@gmail.com>
2023-10-01 22:31:03 -0600

    Update DB Seeds:  Change embedded single apostrophe to double apostrophe
    - To allow for seeding, change apostrophes in resumes, covering letter uploaded text

--- JAMApplication/back_end/db/seeds/03_cover_letters.sql
+++ JAMApplication/back_end/db/seeds/03_cover_letters.sql
@@ -3,50 +3,50 @@
 
 -- Manpreet's Letter (1) --
 INSERT INTO cover_letters (cover_letter_id, user_id, resume_id, contents) VALUES
-  (11, 1, 11, 
-   "Eileen McDonald
+  (11, 1, 11,
+   'Eileen McDonald
     771-555-0199 | Eileen.McDonald@email.com | Des Moines, Iowa 50306
     April 26, 2023
     Frederick & Sons Design Group
 
     Dear Eileen McDonald,
 
-    My name is Manpreet Singh, and I am an enthusiastic and results-driven professional interested in applying for the Web Developer position with Frederick & Sons Design Group. As a Web Developer with over eight years of industry experience in both freelance and full-time positions, I'm familiar with creating and maintaining websites and applications for a variety of clients, and I feel confident I can make positive contributions to your company. I'm excited about the opportunity to apply for a position with a larger organization that devotes time and resources to its team's professional training and development. 
+    My name is Manpreet Singh, and I am an enthusiastic and results-driven professional interested in applying for the Web Developer position with Frederick & Sons Design Group. As a Web Developer with over eight years of industry experience in both freelance and full-time positions, I"m familiar with creating and maintaining websites and applications for a variety of clients, and I feel confident I can make positive contributions to your company. I"m excited about the opportunity to apply for a position with a larger organization that devotes time and resources to its team"s professional training and development.
 
-    I've always been interested in combining my passion for coding and knowledge of SEO content creation to develop sites that attract new users and boost company sales. In the last year, I increased site traffic for my current employer, SkyHigh Developers, by 18% by identifying issues and debugging its site to provide clients with an easier and more enjoyable experience. I'm highly skilled in interpersonal communication and time management and possess a bachelor's degree in computer science. My education provided me with the hands-on training and technical knowledge I use to be successful in my career. 
+    I"ve always been interested in combining my passion for coding and knowledge of SEO content creation to develop sites that attract new users and boost company sales. In the last year, I increased site traffic for my current employer, SkyHigh Developers, by 18% by identifying issues and debugging its site to provide clients with an easier and more enjoyable experience. I"m highly skilled in interpersonal communication and time management and possess a bachelor"s degree in computer science. My education provided me with the hands-on training and technical knowledge I use to be successful in my career.
 
-    As a positive and enthusiastic team member, I'm passionate about supporting my colleagues and am eager to learn from the experienced professionals at Frederick & Sons Design Group. Thank you for considering me for the position and please feel free to contact me if you have questions about my application or want to discuss my credentials further. You can reach me by calling 771-555-0199 or emailing Eileen.McDonald@email.com.
+    As a positive and enthusiastic team member, I"m passionate about supporting my colleagues and am eager to learn from the experienced professionals at Frederick & Sons Design Group. Thank you for considering me for the position and please feel free to contact me if you have questions about my application or want to discuss my credentials further. You can reach me by calling 771-555-0199 or emailing Eileen.McDonald@email.com.
 
     Sincerely,
-    
-    Manpreet Singh"
+
+    Manpreet Singh'
   );
 
 
--- John's Letters (2) --
+-- John"s Letters (2) --
 INSERT INTO cover_letters (cover_letter_id, user_id, resume_id, contents) VALUES
-  (21, 2, 21, 
-   "Erick Kaiser
+  (21, 2, 21,
+   'Erick Kaiser
     Human Resources Manager
     Wakulla Bank of the Coast
     Wakulla, FL
 
     Dear Erick Kaiser,
 
-    My name is John Cloghesy, and I'm currently the Senior Bank Teller at Forgotten Panacea Bank. I'm writing to apply for the open Bank Teller position with Wakulla Bank of the Coast. My 10 years of experience, the fact that Wakulla is my hometown and my desire to work in a smaller community sparked my interest in this role. I have a degree in finance and a degree in accounting, and bank telling is my passion. 
+    My name is John Cloghesy, and I"m currently the Senior Bank Teller at Forgotten Panacea Bank. I"m writing to apply for the open Bank Teller position with Wakulla Bank of the Coast. My 10 years of experience, the fact that Wakulla is my hometown and my desire to work in a smaller community sparked my interest in this role. I have a degree in finance and a degree in accounting, and bank telling is my passion.
 
-    I've spent the last five years as a Senior Bank Teller with my current employer. Here, I've opened the most new savings accounts in the history of the bank using my sales skills. I've also signed off on the second most credit card applications since the bank's inception, using my analytical and critical thinking skills to review prospective customers' ability to pay us back in a timely manner.
+    I"ve spent the last five years as a Senior Bank Teller with my current employer. Here, I"ve opened the most new savings accounts in the history of the bank using my sales skills. I"ve also signed off on the second most credit card applications since the bank"s inception, using my analytical and critical thinking skills to review prospective customers" ability to pay us back in a timely manner.
 
-    Before working with Forgotten Panacea Bank, I served as Chief Bank Teller at Crawfordville First Bank, where I supervised the bank telling staff. I also taught the bank telling staff how to use a completely new banking system using my technology and leadership skills. 
+    Before working with Forgotten Panacea Bank, I served as Chief Bank Teller at Crawfordville First Bank, where I supervised the bank telling staff. I also taught the bank telling staff how to use a completely new banking system using my technology and leadership skills.
 
-    I believe that my documented ability to generate sales and revenue for my employers, in addition to my vast experience and education, makes me a great fit for this Bank Teller role with Wakulla Bank of the Coast. I'm hoping you do, as well. 
+    I believe that my documented ability to generate sales and revenue for my employers, in addition to my vast experience and education, makes me a great fit for this Bank Teller role with Wakulla Bank of the Coast. I"m hoping you do, as well.
 
     Thank you for your consideration,
 
-    John Cloghesy"
+    John Cloghesy'
   ),
-  (22, 2, 22, 
-   "Melissa Rodriguez
+  (22, 2, 22,
+   'Melissa Rodriguez
     771-555-0199 | melissa.rodriguez@email.com | Austin, TX
 
     May 30, 2023
@@ -56,21 +56,21 @@ INSERT INTO cover_letters (cover_letter_id, user_id, resume_id, contents) VALUES
 
     My name is John Cloghesy, and I am writing this letter to express my interest in the Software Developer position at Redshore Digital Creations. I believe that I can offer a significant contribution to your team. My passion for developing efficient, creative software has only grown during my career, and I am excited about the opportunity to showcase my skills.
 
-    From my first internship at Reality Media Company, I've consistently grown as a developer throughout the years. Not only am I dedicated to completing my work promptly and thoroughly, I am also committed to actively learning from every experience I have. During my first internship, I learned about what it takes to succeed in this field, and I've carried that lesson throughout the years as I've worked.
+    From my first internship at Reality Media Company, I"ve consistently grown as a developer throughout the years. Not only am I dedicated to completing my work promptly and thoroughly, I am also committed to actively learning from every experience I have. During my first internship, I learned about what it takes to succeed in this field, and I"ve carried that lesson throughout the years as I"ve worked.
 
     Although I began as a Junior Developer at Ridgewater Software, I eventually earned a position as the Engineering Manager in charge of the digital platforms division. While overseeing this team, our division regularly exceeded user retention goals and grew our subscribers by 10% consistently each quarter. Working in a managerial role taught me many important lessons, but it also helped me realize that my true passion is for creating software. I am excited about the opportunity to bring this passion, experience and talent to Digital Creations.
 
-    Thank you for taking the time to review my application. I'm looking forward to talking to you and learning more about this position. I know I have a lot to offer this company, and I'd love to show you what I can bring to the team.
+    Thank you for taking the time to review my application. I"m looking forward to talking to you and learning more about this position. I know I have a lot to offer this company, and I"d love to show you what I can bring to the team.
 
     Sincerely,
 
-    John Cloghesy"
+    John Cloghesy'
   );
 
 
--- Jane's Letter (1) --
-  (31, 3, 31, 
-  "Brittany Menard
+-- Jane"s Letter (1) --
+  (31, 3, 31,
+   'Brittany Menard
     Fort Wayne, IN
     555-544-0135
     brittany.menard@email.com
@@ -81,13 +81,13 @@ INSERT INTO cover_letters (cover_letter_id, user_id, resume_id, contents) VALUES
 
     My name is Jane Doe, and I am writing to apply for the position of Bartender at Ghost Brewery. I have seven years of experience in various hospitality positions, with four years specifically in bartending. I want to work for Ghost Brewery because of your incredible reputation for quality, creativity and high customer satisfaction.
 
-    I've completed all the training required for Indiana bartenders and hold an active and unrestricted bartending license. I'm also CPR certified. In my current position at Indy Indie Bar, I've completed several training sessions on mixology, including an advanced mixology session and courses in performance bartending. I improved my skills so much that in this last year at Indy Indie Bar, I began teaching several bartending skill classes.
+    I"ve completed all the training required for Indiana bartenders and hold an active and unrestricted bartending license. I"m also CPR certified. In my current position at Indy Indie Bar, I"ve completed several training sessions on mixology, including an advanced mixology session and courses in performance bartending. I improved my skills so much that in this last year at Indy Indie Bar, I began teaching several bartending skill classes.
 
-    My coworkers have described me as hard-working and patient, which are two skills I find extremely useful while bartending. Customer satisfaction surveys also show that I'm well-liked by customers, as I've received the highest scores for 14 separate months during my time at Indy Indie Bar. I'm very proud of this achievement, as customer service is important to me. I know Ghost Brewery shares these values, which is why I would be a great fit for this position.
+    My coworkers have described me as hard-working and patient, which are two skills I find extremely useful while bartending. Customer satisfaction surveys also show that I"m well-liked by customers, as I"ve received the highest scores for 14 separate months during my time at Indy Indie Bar. I"m very proud of this achievement, as customer service is important to me. I know Ghost Brewery shares these values, which is why I would be a great fit for this position.
 
     I appreciate you taking the time to consider my application. Please feel free to call or email me if you have any questions about my qualifications. I am also available to discuss them in person and demonstrate some of my performance bartending skills. I would love the opportunity to join the team at Ghost Brewery.
 
     Sincerely,
 
-    Jane Doe"
+    Jane Doe'
   );
