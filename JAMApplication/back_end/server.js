931302be1fb16167692261dd5d0afa8c3b27648a
John <john.cloghesy@gmail.com>
2023-10-03 17:18:06 -0600

    Server.js File: Update to restructure/organize and reformat server.js file

--- JAMApplication/back_end/server.js
+++ JAMApplication/back_end/server.js
@@ -1,53 +1,98 @@
-// load .env data into process.env
-require('dotenv').config();
+//  =========================================================================
+//  ==-------------------       SERVER.JS      ----------------------------==
+//  =========================================================================
 
-// Web server config
-const sassMiddleware = require('./lib/sass-middleware');
-const express = require('express');
-const morgan = require('morgan');
 
-const PORT = process.env.PORT || 8080;
-const app = express();
+//  ======================   IMPORTS (REQUIRED)    =========================== 
+  const express = require('express');
+  const morgan = require('morgan');
+  const sassMiddleware = require('./lib/sass-middleware');
+  
 
-app.set('view engine', 'ejs');
 
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  '/styles',
-  sassMiddleware({
-    source: __dirname + '/styles',
-    destination: __dirname + '/public/styles',
-    isSass: false, // false => scss, true => sass
-  })
-);
-app.use(express.static('public'));
+//  ======================     CONFIGURATION       ===========================
+//  load .env data       *------------------------------------------------
+  require('dotenv').config();  
 
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-const widgetApiRoutes = require('./routes/widgets-api');
-const usersRoutes = require('./routes/users');
+//  server config        *------------------------------------------------
+  const app = express();
+  const PORT = process.env.PORT || 8080;
 
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-// Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/api/widgets', widgetApiRoutes);
-app.use('/users', usersRoutes);
-// Note: mount other resources here, using the same pattern above
+//  set view engine      *------------------------------------------------
+  app.set('view engine', 'ejs');
 
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
+//  middleware           *------------------------------------------------
+ /** Original Notes
+   * - Load the logger first so all (static) HTTP requests are logged to STDOUT
+   */   
+  app.use(morgan('dev'));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+//  logger output style  *------------------------------------------------
+  /** Original Notes
+   *  - 'dev' - concise output colored by response status for development use
+   *          - the status token will be colored
+   *             - red for server error codes,   yellow for client error codes,
+   *             - cyan for redirection codes,   uncolored for all other codes.
+   */
+  app.use(
+    '/styles',
+    sassMiddleware({
+      source: __dirname + '/styles',
+      destination: __dirname + '/public/styles',
+      isSass: false, // false => scss, true => sass
+    })
+  );
+  
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
+//  ======================  SEPARATE RESOURCES ROUTES   =======================
+  /** Original Notes
+   *   - Separated Routes for each Resource
+   *   - feel free to replace the example routes below with your own
+   */
+//  original routes      *------------------------------------------------
+  const userApiRoutes = require('./routes/api/users-api');
+  const widgetApiRoutes = require('./routes/api/widgets-api');
+  const usersRoutes = require('./routes/users');
+
+//  routes for JAM       *------------------------------------------------
+  const jobApplicationsRoutes = require('./routes/jobApplications');
+
+//  ======================   MOUNT ALL RESOURCES ROUTES  =======================
+  /** Original Notes
+   *  - Feel free to replace the example routes below with your own
+   *  - Endpoints that return data (eg. JSON) usually start with `/api`
+   *  - mount other resources here using the same pattern as below
+   */
+
+//  example resources    *------------------------------------------------
+    app.use('/api/users', userApiRoutes);
+    app.use('/api/widgets', widgetApiRoutes);
+    app.use('/users', usersRoutes);
+    app.use('/api/jobApplication', jobApplicationRoutes);
+    
+
+//  resources for JAM    *------------------------------------------------
+    app.use('/jobApplications', jobApplicationRoutes);
+
+
+//  ======================     HOME PAGE          ==========================
+  /**
+   * Home page
+   *  - Warning: avoid creating more routes in this file!
+   *  - Separate them into separate routes files (see above)
+   */
+
+//  app.get              *------------------------------------------------
+  app.get('/', (req, res) => {
+    res.render('index');
+  });
+
+  
+//  app.listen           *------------------------------------------------
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
+//  =========================================================================
+//  =========================================================================
