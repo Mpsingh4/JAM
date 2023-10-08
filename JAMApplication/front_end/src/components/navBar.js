e98c04a825c03eb41408b8e04cc6c3e30cde64a0
Manpreet Saini <mp_manpreet@hotmail.com>
2023-10-08 17:49:01 -0400

    minor cleanup

--- JAMApplication/front_end/src/components/navBar.js
+++ JAMApplication/front_end/src/components/navBar.js
@@ -1,14 +1,10 @@
 import React, { useState } from 'react'
-import Home from './home';
-import { BsCart2 } from "react-icons";
 import Logo from "../assets/jamLogo3.png";
 import HomeIcon from "@mui/icons-material/Home"
 import LoginIcon from "@mui/icons-material/Login"
 import RegisterIcon from "@mui/icons-material/AppRegistration"
 import AboutIcon from "@mui/icons-material/QuestionMark"
 import { HiOutlineBars3 } from "react-icons/hi2"
-import Drawer from "@mui/material/Drawer";
-import { Link } from 'react-router-dom'
 import LoginButton from './login';
 
 const NavBar = () => {
